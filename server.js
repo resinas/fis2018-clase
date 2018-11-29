2fbc8ed3cbfcddad5b1335d68e503f3b58a02ced
Manuel Resinas <resinas@us.es>
2018-11-29 06:18:59 +0100

    Added some tests using mocks and stub

---
+++ server.js
@@ -0,0 +1,165 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var DataStore = require('nedb');
+var cors = require('cors');
+var path = require('path') 
+
+const CONTACTS_APP_DIR = "/dist/contacts-app"; 
+var BASE_API_PATH = "/api/v1";
+var filename = __dirname + "/contacts.json";
+
+var contacts = [
+    {"name": "juan", "phone": 5555}
+];
+
+var db = new DataStore({
+    filename: filename,
+    autoload: true
+});
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, CONTACTS_APP_DIR))); 
+app.get('/', function(req, res) { 
+    res.sendFile(path.join(__dirname, CONTACTS_APP_DIR, '/index.html')); 
+}); 
+
+
+app.get(BASE_API_PATH + "/contacts", (req, res) => {
+    db.find({}, (err, contacts) => {
+        if (err) {
+            console.error("Error accessing database");
+            res.sendStatus(500);
+        } else {
+            res.send(contacts.map((contact) => {
+                delete contact._id;
+                return contact;
+            }));
+        }
+    });
+});
+
+
+app.post(BASE_API_PATH + "/contacts", (req, res) => {
+    // Create a new contact
+    console.log(Date()+" - POST /contacts");
+    var contact = req.body;
+    db.insert(contact);
+    res.sendStatus(201);
+});
+
+app.put(BASE_API_PATH + "/contacts", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - PUT /contacts");
+    res.sendStatus(405);
+});
+
+app.delete(BASE_API_PATH + "/contacts", (req, res) => {
+    // Remove all contacts
+    console.log(Date()+" - DELETE /contacts");
+    db.remove({});    
+    res.sendStatus(200);
+});
+
+
+app.post(BASE_API_PATH + "/contacts/:name", (req, res) => {
+    // Forbidden
+    console.log(Date()+" - POST /contacts");
+    res.sendStatus(405);
+});
+
+
+
+app.get(BASE_API_PATH + "/contacts/:name", (req, res) => {
+    // Get a single contact
+    var name = req.params.name;
+    console.log(Date()+" - GET /contacts/"+name);
+
+    db.find({"name": name},(err,contacts)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(contacts.length>1){
+                console.warn("Incosistent DB: duplicated name");
+            }
+            res.send(contacts.map((contact)=>{
+                delete contact._id;
+                return contact;
+            })[0]);
+        }
+    });
+});
+
+
+app.delete(BASE_API_PATH + "/contacts/:name", (req, res) => {
+    // Delete a single contact
+    var name = req.params.name;
+    console.log(Date()+" - DELETE /contacts/"+name);
+
+    db.remove({"name": name},{},(err,numRemoved)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(numRemoved>1){
+                console.warn("Incosistent DB: duplicated name");
+            }else if(numRemoved == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    });
+});
+app.delete(BASE_API_PATH + "/contacts/:name", (req, res) => {
+    // Delete a single contact
+    var name = req.params.name;
+    console.log(Date()+" - DELETE /contacts/"+name);
+
+    db.remove({"name": name},{},(err,numRemoved)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(numRemoved>1){
+                console.warn("Incosistent DB: duplicated name");
+            }else if(numRemoved == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    });
+});
+
+app.put(BASE_API_PATH + "/contacts/:name", (req, res) => {
+    // Update contact
+    var name = req.params.name;
+    var updatedContact = req.body;
+    console.log(Date()+" - PUT /contacts/"+name);
+
+    if(name != updatedContact.name){
+        res.sendStatus(409);
+        return;
+    }
+
+    db.update({"name": name},updatedContact,(err,numUpdated)=>{
+        if(err){
+            console.error("Error accesing DB");
+            res.sendStatus(500);
+        }else{
+            if(numUpdated>1){
+                console.warn("Incosistent DB: duplicated name");
+            }else if(numUpdated == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    });
+});
+
+module.exports.app = app;
+module.exports.db = db;
--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,10 @@
-
+var server = require('../server');
 var chai = require('chai');
+var chaiHttp = require('chai-http');
+var sinon = require('sinon');
 var expect = chai.expect;
 
+chai.use(chaiHttp);
 
 describe('Contacts API', () => {
     it('hola mundo de prueba', (done) => {
@@ -12,5 +15,60 @@ describe('Contacts API', () => {
 
         expect(resultado).to.equal(8);
         done();
+    });
+
+    before((done) => {
+        var contacts = [
+            {"name": "juan", "phone": 5555},
+            {"name": "pepe", "phone": 6666}
+        ];
+
+        var dbFindStub = sinon.stub(server.db, 'find');
+        dbFindStub.yields(null, contacts);
+
+        done();
+    });
+
+    describe('GET /', () => {
+        it('should return HTML', (done) => {
+            chai.request(server.app)
+                .get('/')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.html;
+                    done();
+                });        
+        });
+    });
+
+    describe('GET /contacts', () => {
+        it('should return all contacts', (done) => {
+            chai.request(server.app)
+                .get('/api/v1/contacts')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.an('array');
+                    expect(res.body).to.have.lengthOf(2);
+                    done();
+                });  
+        });
+    });
+
+    describe('POST /contacts', () => {
+        it('should create a new contact', (done) => {
+            var contact = {"name": "jaime", "phone":1111};
+            var dbMock = sinon.mock(server.db);
+            dbMock.expects('insert').withArgs(contact);
+    
+            chai.request(server.app)
+                .post('/api/v1/contacts')
+                .send(contact)
+                .end((err, res) => {
+                    expect(res).to.have.status(201);
+                    dbMock.verify();
+                    done();
+                });  
+
+        });
     });
 });
