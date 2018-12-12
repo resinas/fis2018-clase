a87f23f89fb70d248c0141cc3a751bbd30be98d3
Manuel Resinas <resinas@us.es>
2018-12-12 17:07:29 +0100

    Add circuit breaker to request

--- client-demo/contactsResource.js
+++ client-demo/contactsResource.js
@@ -1,16 +1,27 @@
 var urljoin = require('url-join');
 var request = require('request-promise-native').defaults({json: true});
+var CommandFactory = require('hystrixjs').commandFactory;
 var contactsServer = (process.env.CONTACTS_URL || 'http://localhost:3000/api/v1');
-var contactsKey = 'aaae1676-0b9a-4b65-bf41-975929bf93c5';
+var contactsKey = 'aae1676-0b9a-4b65-bf41-975929bf93c5';
 
 function contactsResource(url) {
     return urljoin(contactsServer, url, '?apikey='+contactsKey);
 }
 
-function getAllContacts() {
+function getAllContactsBase() {
     var url = contactsResource("/contacts");
     console.log(url);
     return request.get(url);
+}
+
+
+var getAllContactsCommand = CommandFactory.getOrCreate("Get Contacts")
+    .run(getAllContactsBase)
+    .timeout(100)
+    .build()
+
+function getAllContacts() {
+    return getAllContactsCommand.execute();
 }
 
 module.exports = {
