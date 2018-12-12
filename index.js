bfd5af31d3aaef3a3b9b10b3bbf8eaeb6f06f6ab
Manuel Resinas <resinas@us.es>
2018-12-12 19:07:02 +0100

    Add new tests and environment variable

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var server = require('./server');
 var mongoose = require('mongoose');
 var port = (process.env.PORT || 3000);
+var dbUrl = (process.env.DB || 'mongodb://localhost/test');
 
 console.log("Starting API server...");
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(dbUrl);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
