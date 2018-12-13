a15dbe76c24f1e2c94694c8abcfdda721bdfe859
Manuel Resinas <resinas@us.es>
2018-12-13 09:54:49 +0100

    Add apikey as environment variable

--- client-demo/contactsResource.js
+++ client-demo/contactsResource.js
@@ -2,7 +2,7 @@ var urljoin = require('url-join');
 var request = require('request-promise-native').defaults({json: true});
 var CommandFactory = require('hystrixjs').commandFactory;
 var contactsServer = (process.env.CONTACTS_URL || 'http://localhost:3000/api/v1');
-var contactsKey = 'aae1676-0b9a-4b65-bf41-975929bf93c5';
+var contactsKey = (process.env.CONTACTS_APIKEY || 'aae1676-0b9a-4b65-bf41-975929bf93c5');
 
 function contactsResource(url) {
     return urljoin(contactsServer, url, '?apikey='+contactsKey);
